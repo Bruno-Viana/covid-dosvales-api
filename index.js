45a7e2179b07228d75aaaf4064ce0247a8b80f81
Bruno Viana <79768022+Bruno-Viana@users.noreply.github.com>
2021-03-19 11:44:20 -0300

    fixes dyno behaviour

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const scraper = require ("./scrap");
 let port = process.env.PORT || 3000;
 var cors = require('cors')
+const schedule = require('node-schedule');
 const redis = require("redis");
 const client = redis.createClient(process.env.REDIS_URL);
-let importData = '';
+let importData = [];
 
 
 //Adicionar ao Heroku em Buildpack: https://buildpack-registry.s3.amazonaws.com/buildpacks/jontewks/puppeteer.tgz
@@ -16,20 +17,17 @@ let importData = '';
 app.get("/", (req, res) =>{
     res.send("/api/dados para acessar a api.");
 });
-
-
-app.get('/api/dados', cors(), function (req, res, next) {
-    client.get('dadosx', (err, reply) => {
+client.get('dadosx', (err, reply) => {
     if (err) throw err;
-    console.log('reply' + reply)
     importData=reply
-    console.log('import data' + importData)
-    });
+    console.log('Data importada:', reply)
+})
+app.get('/api/dados', cors(), function (req, res, next) {
     res.send(importData)
 })
 
 app.use(function(req,res,next){
-	res.header('Access-Control-Allow-Origin', "http://localhost:3001");
+	res.header('Access-Control-Allow-Origin', "http://localhost:3000");
     res.header('Access-Control-Allow-Methods', 'GET');
 	res.header('Access-Control-Allow-Headers', "Content-Type");
     next();
@@ -38,3 +36,17 @@ app.use(function(req,res,next){
 app.listen(port, () =>{
     console.log("Rodando na porta" + port)
 });
+
+schedule.scheduleJob('0 */1 * * *', function(){
+    scraper;
+    console.log('Entrou Scheduler')
+    client.get('dadosx', (err, reply) => {
+        if (err) throw err;
+        importData=reply
+        console.log('Novos dados apos scraping')
+    })
+    console.log('Refresh de dados')
+    app.get('/api/dados', cors(), function (req, res, next) {
+        res.send(importData)
+    })
+  });
